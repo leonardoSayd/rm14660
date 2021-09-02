afbc5fcd3466599cc68a10d7b87f69d22fa72445
leonardoSayd <89795826+leonardoSayd@users.noreply.github.com>
2021-09-02 11:21:32 -0300

    14660

---
+++ index.js
@@ -0,0 +1,18 @@
+//servidor 
+var express = require('express')
+var app = express()
+
+var porta = 3030 
+
+// configurações 
+app.set("view engine",'ejs')
+app.use(express.static('./'))
+
+//rota para abrir o arquivo index.ejs
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+//ligar o servidr 
+app.listen(porta)
+  
