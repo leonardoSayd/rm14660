7f0e508013f35fa8480b410a2fca07cd51ab5084
leonardoSayd <89795826+leonardoSayd@users.noreply.github.com>
2021-09-27 12:34:47 -0300

    Delete s.js

--- s.js
+++
@@ -1,4 +0,0 @@
-var mongoose = require('mongoose')
-var conexao = ()=>{
-    var camninho = mongoose.connect('mongodb+srv://awesome:leon0708@fiap.tdbqu.mongodb.net/mongoatlas?retryWrites=true&w=majority')
-}
